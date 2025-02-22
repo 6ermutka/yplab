e13f5c54cc0125b91a86a84e8eb065ac5aded7a1
6ermutka <129321411+6ermutka@users.noreply.github.com>
2025-02-22 13:53:39 +0500

    Add files via upload

---
+++ lab1/Program2.fs
@@ -0,0 +1,20 @@
+﻿//Найти первую цифру натурального числа.
+open System
+
+let rec check() =
+    let input = Console.ReadLine()
+    match Int32.TryParse(input) with
+    | (true, a) when a > 0 -> a
+    | (false, _) ->
+        printfn "Вы ввели не натуральное число. Попробуйте еще раз:"
+        check()
+    | (_, _) ->
+        printfn "Вы ввели не натуральное число. Попробуйте еще раз:"
+        check()
+printf "Введите натуральное число: "
+let y = check()
+let rec ff y=
+    if y < 10 then (y%10)
+    else ff (y/10) 
+printf "Первая цифра %i = %i " (y) (ff y)
+    
