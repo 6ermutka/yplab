51164c5413f8049c613aaca22cbaef2183066a9c
6ermutka <129321411+6ermutka@users.noreply.github.com>
2025-02-22 13:31:28 +0500

    Add files via upload

---
+++ lab1/Program1.fs
@@ -0,0 +1,12 @@
+﻿//Сформировать список из степеней числа 2.
+open System
+
+printf "Введите максимальную степень двойки): "
+let y = int(Console.ReadLine())
+let rec db x i =
+    if i = 0 then 1
+    else 2 * db (2) (i-1)
+let x =
+    [ for i in 0 .. y do
+        yield db 2 i ]
+printfn "Список из степеней числа 2: %A" x
