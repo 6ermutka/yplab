2034ead053a6584d9b91e0fd185797ea7ba48fa0
6ermutka <129321411+6ermutka@users.noreply.github.com>
2025-03-09 00:30:31 +0500

    Add files via upload

---
+++ lab3/Program.fs
@@ -0,0 +1,41 @@
+﻿(*Получить список из минимальных цифр натуральных чисел, содержащихся в
+исходной последовательности*)
+open System
+let rec check() =
+    let input = Console.ReadLine()
+    match Int32.TryParse(input) with
+    | (true, a) when a > 0 -> a
+    | _ ->
+        printfn "Вы ввели не натуральное число. Попробуйте еще раз:"
+        check()
+let p = lazy (
+    printf "Введите кол-во элементов в исходной последовательности: ";
+    let n = check();
+    n
+)
+let rec fmin n cmin =
+    if n = 0 then cmin
+    else
+        let d = n % 10
+        if d < cmin then fmin (n / 10) d
+        else fmin (n / 10) cmin
+let minDigit n = fmin n 9
+let n = p.Value
+let x = seq { 
+    for i in 1 .. n do
+        printf "Введите элемент %d: " i
+        yield check() 
+}
+let c = Seq.map minDigit x
+let v = Seq.toList c
+printf "Последовательность минимальных цифр %A" v
+
+
+        
+        
+        
+        
+            
+            
+            
+    
