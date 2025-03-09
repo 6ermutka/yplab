66605980310931e973a8116d5d11d0e5090bf174
6ermutka <129321411+6ermutka@users.noreply.github.com>
2025-03-09 13:45:36 +0500

    Add files via upload

---
+++ lab3/Program3.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.IO
+
+let rec getAllFiles directory =
+    seq {
+        yield! Directory.GetFiles(directory)
+        for subDirectory in Directory.GetDirectories(directory) do
+            yield! getAllFiles subDirectory
+    }
+printf "Введите путь к каталогу: "
+let directoryPath = Console.ReadLine()
+let files = lazy (getAllFiles directoryPath)
+printfn "Получаю файлы из католога %s" directoryPath
+if Seq.isEmpty files.Value then 
+    printfn "В каталоге нет файлов."
+else
+    let shortestFileName = files.Value |> Seq.minBy (fun file -> Path.GetFileName(file).Length)
+    printfn "Самое короткое название файла: %s" (Path.GetFileName(shortestFileName))
+    printfn "Путь до этого файла: %s" (shortestFileName)
+            
+    
