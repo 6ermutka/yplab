382ce496e34012db78fc0063a70d84f76ebcfac3
6ermutka <129321411+6ermutka@users.noreply.github.com>
2025-03-21 20:20:56 +0400

    Create program1.pl

---
+++ lab5/program1.pl
@@ -0,0 +1,17 @@
+% предикат для запуска программы
+start :-
+    write('Введите число: '),
+    read(N),
+    reverse_number(N, 0, Reversed),
+    write('Перевернутое число: '), 
+    write(Reversed), 
+    nl.
+% базис рекурсии
+reverse_number(0, Acc, Acc).
+% рекурсия 
+reverse_number(N, Acc, Reversed) :-
+    N > 0,
+    LastDigit is N mod 10,          
+    NewAcc is Acc * 10 + LastDigit, 
+    NewN is N // 10,                
+    reverse_number(NewN, NewAcc, Reversed).
