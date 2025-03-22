52b77121d6ac8e9fbe054de8c4f56de9a7f15b33
6ermutka <129321411+6ermutka@users.noreply.github.com>
2025-03-22 11:28:07 +0400

    Create program3.pl

---
+++ lab5/program3.pl
@@ -0,0 +1,13 @@
+% предикат для запуска программы
+start :-
+    write('Введите универсальное множество U [элемент1, элемент2, ...]: '), nl,
+    read(U),
+    write('Введите множество A [элемент1, элемент2, ...]: '), nl,
+    read(A),
+    complement(U, A, Complement),
+    write('Дополнение множества A: '), write(Complement), nl.
+member(X, [X|_]).
+member(X, [_|T]) :- member(X, T).
+% предикат для нахождения дополнения множества
+complement(U, A, Complement) :-
+    findall(X, (member(X, U), \+ member(X, A)), Complement).
