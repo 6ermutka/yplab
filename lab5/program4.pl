305c93c3bfc92e0a56d8302ed362dfe8e71ee0d8
6ermutka <129321411+6ermutka@users.noreply.github.com>
2025-03-23 11:03:21 +0400

    Update program4.pl

--- lab5/program4.pl
+++ lab5/program4.pl
@@ -2,10 +2,10 @@ color(blue).
 color(black).
 brand(buick).
 brand(ford_mustang).
-brand(toyota).
+brand(kraisler).
 car(Color, Brand) :-
     color(Color),
     brand(Brand),
     (Color = blue; Brand = buick),
     (Color \= blue; Brand = ford_mustang),
-    (Color \= blue; Brand = toyota).
+    (Color \= blue; Brand = kraisler).
